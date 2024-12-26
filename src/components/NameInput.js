23e62b102f67f0c6f72812d7572884c86b698dc8
Duy Khang Bùi Ngọc <khangngoc3082005@gmail.com>
2024-12-27 00:44:59 +0700

    khangjoke

--- src/components/NameInput.js
+++ src/components/NameInput.js
@@ -1,43 +1,57 @@
 import { Button, Input } from "antd";
 import React from "react";
 
+export default function NameInput({
+  inputValue,
+  nameList,
+  setInputValue,
+  setNameList,
+  setTeam,
+}) {
+  const onValueChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-export default function NameInput({inputValue, nameList, setInputValue, setNameList, setTeam}) {
-
-
-    const onValueChange = (e) => {
-        setInputValue(e.target.value)
+  const handleRandomize = () => {
+    if (!inputValue.trim()) {
+      console.error("Input is empty! Please enter some names.");
+      return;
     }
 
-    const handleRandomize = () => {
-        if (!inputValue.trim()) {
-            console.error("Input is empty! Please enter some names.");
-            return;
-        }
-
-
-        const nameInputList = inputValue.split(/[\n,]+/).map((name) => name.trim()).filter((name) => name);
-        console.log(nameInputList)
-        for (let i = 9; i > 0; i--) {
-            const j = Math.floor(Math.random() * (1 + i));
-            [nameInputList[i], nameInputList[j]] = [nameInputList[j], nameInputList[i]]
-        }
-
-        console.log(nameInputList);
-
-        const team1 = nameInputList.slice(0, 5);
-        const team2 = nameInputList.slice(5);
-        
-        setTeam(team1, team2);
-
+    const nameInputList = inputValue
+      .split(/[\n,]+/)
+      .map((name) => name.trim())
+      .filter((name) => name);
+    console.log(nameInputList);
+    for (let i = 9; i > 0; i--) {
+      const j = Math.floor(Math.random() * (1 + i));
+      [nameInputList[i], nameInputList[j]] = [
+        nameInputList[j],
+        nameInputList[i],
+      ];
     }
 
+    console.log(nameInputList);
 
+    const team1 = nameInputList.slice(0, 5);
+    const team2 = nameInputList.slice(5);
 
-    return(
-        <div>
-            <Input.TextArea value={inputValue} onChange={(e) => onValueChange(e)}></Input.TextArea>
-            <Button className="m-1" onClick={handleRandomize}>Random</Button>
-        </div>
-    )
+    setTeam(team1, team2);
+  };
+
+  return (
+    <div>
+      <Input.TextArea
+        className="max-h-60 min-h-40"
+        value={inputValue}
+        onChange={(e) => onValueChange(e)}
+      ></Input.TextArea>
+      <Button
+        className="m-1 font-semibold hover:bg-[#feffc6]"
+        onClick={handleRandomize}
+      >
+        RANDOM
+      </Button>
+    </div>
+  );
 }
--- src/components/TeamDisplay.js
+++ src/components/TeamDisplay.js
@@ -1,22 +1,21 @@
 import { List } from "antd";
 import React from "react";
 
-export default function TeamDisplay({team}) {
+export default function TeamDisplay({ team }) {
+  const data = team.map((name, index) => ({ name: name, id: index + 1 }));
 
-    const data = team.map((name, index) => ({name: name , id: index + 1}) )
-
-    return(
-        <div>
-            <List
-                itemLayout="horizontal"
-                dataSource={data}
-                bordered = {true}
-                renderItem={(item) => (
-                <List.Item key = {item.id}>
-                    <div>{item.name}</div>
-                </List.Item>
-                )}
-            />
-        </div>
-    )
+  return (
+    <div className="backdrop-opacity-20 backdrop-invert bg-white/30 rounded-md">
+      <List
+        itemLayout="horizontal"
+        dataSource={data}
+        bordered={true}
+        renderItem={(item) => (
+          <List.Item key={item.id}>
+            <div className="font-semibold">{item.name}</div>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
 }
